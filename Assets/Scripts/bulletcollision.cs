d7c175c0dd1a11263622bfec6acd3d61b79667e5
Draven Stedman <dravenstedman@gmail.com>
2019-02-19 16:37:19 -0600

    first commit in git; so far we got movement and basic shooting.

---
+++ Assets/Scripts/bulletcollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * this code was written by draven r stedman
+ * this code spits in the face of copyright and
+ * capitalism's other dead gods, and is licensed
+ * under both the GNU General Public License v3
+ * and the Crimethinc. N©! license. see
+ * license.txt for details.
+ */
+
+public class bulletcollision : MonoBehaviour {
+	void Start () {}
+	void Update () {}
+
+	void OnCollisionEnter(Collision collision){
+		// once we get this to a point where we need to detect collision between bullets and players we'll do that here but for now we'll just make them stop dead in their tracks.
+		Debug.Log("Collision.");
+		Rigidbody body = GetComponent<Rigidbody>();
+		body.velocity = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/shooty.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * this code was written by draven r stedman
+ * this code spits in the face of copyright and
+ * capitalism's other dead gods, and is licensed
+ * under both the GNU General Public License v3
+ * and the Crimethinc. N©! license. see
+ * license.txt for details.
+ */
+
+public class shooty : MonoBehaviour {
+
+	public GameObject origbullet;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			shoot ();
+		}
+	}
+
+	void shoot ()
+	{
+		GameObject tempbullet = Instantiate( origbullet, transform.position, transform.rotation ) as GameObject;
+		AudioSource pew = GetComponent<AudioSource> ();
+		pew.Play ();
+		Rigidbody projectile = tempbullet.GetComponent<Rigidbody>();
+		projectile.velocity = Camera.main.transform.forward * 20f;
+	}
+}
